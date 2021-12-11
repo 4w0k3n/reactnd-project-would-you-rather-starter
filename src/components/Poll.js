81f1feb34508bc2b28e130d6c12fa4e2c6f75012
Paul Mack <paul.mack@awkn.eu>
2021-12-11 15:32:16 +0100

    finished voting and show result mechanism

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import {Badge, Card, Col, Container, Figure, Row, Button} from "react-bootstrap";
+import {Badge, Card, Col, Container, Figure, Row, Button, ProgressBar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {handleSaveAnswer} from "../state/shared/sharedActions";
+import {AiOutlineUser, MdHowToVote, MdOutlineWhereToVote} from "react-icons/all";
 
 class Poll extends Component {
     render() {
@@ -19,22 +20,99 @@ class Poll extends Component {
                         <Figure.Caption>
                             @{this.props.poll.author} wants to know...
                         </Figure.Caption>
-                        <span className='text-muted' style={{fontSize: '10px'}}>{new Date(this.props.poll.timestamp).toLocaleString()}</span>
+                        <span className='text-muted'
+                              style={{fontSize: '10px'}}>{new Date(this.props.poll.timestamp).toLocaleString()}</span>
                     </Figure>
                 </Row>
                 <Row className="justify-content-md-center">
                     <Card style={{width: '50%'}}>
                         <Card.Body>
                             <Card.Title style={{margin: '1rem'}}>Would you rather...</Card.Title>
-                                <Row>
-                                    <Col><button className='btn btn-lg btn-outline-primary'  style={{margin: '1rem'}} onClick={event => {this.handleAnswer(event, 'optionOne')}}>{this.props.poll.optionOne.text}</button></Col>
-                                </Row>
-                                <Row>
-                                    <Col><button className='btn btn-lg btn-outline-primary'  style={{margin: '1rem'}} onClick={event => {this.handleAnswer(event, 'optionTwo')}}>{this.props.poll.optionTwo.text}</button></Col>
-                                </Row>
-                                <Row>
-                                    <Col><Button as={Link} variant="outline-dark"  style={{margin: '1rem'}} to={'/'}>GO BACK</Button></Col>
-                                </Row>
+                            {!this.props.answered ?
+                                (<div>
+                                        <Row>
+                                            <Col>
+                                                <button className='btn btn-lg btn-outline-primary'
+                                                        style={{margin: '1rem'}} onClick={event => {
+                                                    this.handleAnswer(event, 'optionOne')
+                                                }}>{this.props.poll.optionOne.text}</button>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <button className='btn btn-lg btn-outline-primary'
+                                                        style={{margin: '1rem'}} onClick={event => {
+                                                    this.handleAnswer(event, 'optionTwo')
+                                                }}>{this.props.poll.optionTwo.text}</button>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col><Button as={Link} variant="outline-dark" style={{margin: '1rem'}}
+                                                         to={'/'}>GO BACK</Button></Col>
+                                        </Row>
+                                    </div>
+                                ) :
+                                (
+                                    <div>
+                                        <Row>
+                                            <span className='text-muted' style={{marginTop: '2rem'}}>{this.props.poll.optionOne.text}
+                                                {this.props.answered && this.props.answeredOption === 'optionOne' &&
+                                                    <span className='text-success'>
+                                                        <MdOutlineWhereToVote style={{marginLeft: 20, fontSize:'30px'}}/>
+                                                    </span>
+                                                }
+                                            </span>
+                                        </Row>
+                                        <Row>
+                                            <Col> <ProgressBar animated label={this.props.questions[this.props.id].optionOne.votes.length + ` vote(s)`}
+                                                               now={((this.props.questions[this.props.id].optionOne.votes.length)
+                                                                   / (this.props.questions[this.props.id].optionOne.votes.length
+                                                                       + this.props.questions[this.props.id].optionTwo.votes.length))*100}
+                                                               style={{margin: '1rem', fontSize: '15px'}}/> </Col>
+                                        </Row>
+                                        <Row>
+                                            <span className='text-muted' style={{marginTop: '2rem'}}>{this.props.poll.optionTwo.text}
+                                                {this.props.answered && this.props.answeredOption === 'optionTwo' &&
+                                                    <span className='text-success'>
+                                                        <MdOutlineWhereToVote style={{marginLeft: 20, fontSize:'30px'}}/>
+                                                    </span>
+                                                }
+                                            </span>
+                                        </Row>
+                                        <Row>
+                                            <Col> <ProgressBar animated label={this.props.questions[this.props.id].optionTwo.votes.length + ` vote(s)`}
+                                                               now={((this.props.questions[this.props.id].optionTwo.votes.length)
+                                                                   / (this.props.questions[this.props.id].optionOne.votes.length
+                                                                       + this.props.questions[this.props.id].optionTwo.votes.length))*100}
+                                                               style={{margin: '1rem', fontSize: '15px'}}/> </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                { this.props.answered && this.props.answeredOption === 'optionOne' &&
+                                                    (
+                                                        <p style={{marginTop: '2rem'}}>You have answered like {((this.props.questions[this.props.id].optionTwo.votes.length)
+                                                            / (this.props.questions[this.props.id].optionOne.votes.length
+                                                                + this.props.questions[this.props.id].optionTwo.votes.length))*100} %.
+                                                        </p>
+                                                    )
+                                                }
+                                                { this.props.answered && this.props.answeredOption === 'optionTwo' &&
+                                                    (
+                                                        <p style={{marginTop: '2rem'}}>You have answered like {((this.props.questions[this.props.id].optionTwo.votes.length)
+                                                            / (this.props.questions[this.props.id].optionOne.votes.length
+                                                                + this.props.questions[this.props.id].optionTwo.votes.length))*100} %.
+                                                        </p>
+                                                    )
+                                                }
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col><Button as={Link} variant="outline-dark" style={{margin: '1rem'}}
+                                                         to={'/'}>GO BACK</Button></Col>
+                                        </Row>
+                                    </div>
+                                )
+                            }
                         </Card.Body>
                     </Card>
                 </Row>
@@ -52,6 +130,7 @@ function mapStateToProps({authedUser, questions, users}, {id}) {
     return {
         answered: Object.keys(users[authedUser].answers).includes(id) && !Object.keys(questions)
             .filter(key => !Object.keys(users[authedUser].answers).includes(key)).includes(id),
+        answeredOption: users[authedUser].answers[id],
         poll: questions[id],
         questions,
         authedUser,
